262c58c8aa0c825a138893571b19244110de4176
Hieu <hieudnguyen31082007@gmail.com>
2026-05-22 15:59:27 +0700

    unfolow server, common

--- common/src/main/java/org/example/model/user/IBidder.java
+++
@@ -1,9 +0,0 @@
-package org.example.model.user;
-
-/**
- * Interface defining the behaviors of a bidder.
- */
-public interface IBidder {
-    void placeBid(int productId, long amount);
-    // Có thể thêm các method như viewMyBids(),...
-}
